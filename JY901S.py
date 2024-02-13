ad3bfea5a476bc0663ebda64380186e37b039f0e
Andrew Humphrey <digiwhale.humphrey@gmail.com>
2024-02-12 21:24:24 -0500

    1

--- JY901S.py
+++ JY901S.py
@@ -529,7 +529,7 @@ def readConfig(device, regAddr, regCount):
     """
     tVals = device.readReg(regAddr, regCount)
     if (len(tVals) > 0):
-        print("Resultx: ", tVals)
+        print("Resultx: ", [hex(byte) for byte in tVals])
     else:
         print("No response")
     # tVals = device.readReg(0x23, 2)
