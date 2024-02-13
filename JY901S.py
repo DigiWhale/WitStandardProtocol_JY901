a09aaa20fe4f680725a7615bae45035ed19f9717
Andrew Humphrey <digiwhale.humphrey@gmail.com>
2024-02-13 12:51:41 -0500

    1

--- JY901S.py
+++ JY901S.py
@@ -638,8 +638,9 @@ def setConfig(device):
     print(RED + "Setting gyro static threshold" + RESET)
     error = device.readReg(0x6A, 2, "WERROR")
     werror = (error[1]/1000*180/3.1415926)*1.2
-    print("WERROR: ", werror)
-    device.writeReg(0x61, werror)
+    hexwerror = hex(struct.unpack('<I', struct.pack('<f', werror))[0])
+    print("WERROR: ", hexwerror)
+    device.writeReg(0x61, hexwerror)
     time.sleep(0.1)
     print(RED + "Saving..." + RESET)
     device.save()
