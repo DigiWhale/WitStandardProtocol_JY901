1539caeb52feb9d67662d60b04962c7eab36c494
Andrew Humphrey <digiwhale.humphrey@gmail.com>
2024-02-12 22:10:44 -0500

    1

--- JY901S.py
+++ JY901S.py
@@ -621,6 +621,7 @@ if __name__ == '__main__':
     readConfig(device, 0x5F, 1)
     setConfig(device)  
     FiledCalibration(device)                            
+    readConfig(device, 0x5F, 1)
     device.dataProcessor.onVarChanged.append(onUpdate)                             
     input()
     device.closeDevice()
