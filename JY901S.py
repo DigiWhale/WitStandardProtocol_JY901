8f7f57b3de8f5ab3cc7a38278e57b11bd3a127fb
Andrew Humphrey <digiwhale.humphrey@gmail.com>
2024-02-13 13:56:33 -0500

    1

--- JY901S.py
+++ JY901S.py
@@ -682,13 +682,13 @@ def readCompassConfig(device):
     :return:
     """
     print("Reading compass configuration")
-    print("Output frequency: ", device.readReg(0x03, 1)[0])
-    print("Output mode: ", device.readReg(0x02, 1)[0])
-    print("Orientation: ", device.readReg(0x23, 1)[0])
-    print("Mode: ", device.readReg(0x24, 1)[0])
-    print("K-value: ", device.readReg(0x25, 1)[0])
-    print("Acceleration filter: ", device.readReg(0x2A, 1)[0])
-    print("Send data on power on: ", device.readReg(0x2D, 1)[0])
+    print("Output frequency: ", hex(device.readReg(0x03, 1)[0] & 0xFFFF))
+    print("Output mode: ", hex(device.readReg(0x02, 1)[0] & 0xFFFF))
+    print("Orientation: ", hex(device.readReg(0x23, 1)[0] & 0xFFFF))
+    print("Mode: ", hex(device.readReg(0x24, 1)[0] & 0xFFFF))
+    print("K-value: ", hex(device.readReg(0x25, 1)[0] & 0xFFFF))
+    print("Acceleration filter: ", hex(device.readReg(0x2A, 1)[0] & 0xFFFF))
+    print("Send data on power on: ", hex(device.readReg(0x2D, 1)[0] & 0xFFFF))
     # data = device.readReg(0x04, 1, "Mode")
     # if (len(data) > 0):
     #     print("Mode: ", data)
